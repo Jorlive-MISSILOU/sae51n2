839b86b35d70ef04fba03d5fa37ff7e1b278f211
user <user@debjo>
2023-11-27 14:21:53 +0100

    conf.php

--- dolibarr/conf.php
+++ dolibarr/conf.php
@@ -1,6 +1,6 @@
 <?php
 $dolibarr_main_db_type = 'mysqli';
-$dolibarr_main_db_host = 'localhost';
+$dolibarr_main_db_host = 'sae51n2_1';
 $dolibarr_main_db_port = '3306';
 $dolibarr_main_db_user = 'dolibarr';
 $dolibarr_main_db_pass = 'dolibarr';
