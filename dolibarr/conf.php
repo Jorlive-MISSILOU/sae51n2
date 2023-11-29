0ee574c3049dabbd15af1ecfea4e0d3d3b517bf8
Jorlive-MISSILOU <jorlivemissilou00@gmail.com>
2023-11-29 12:25:49 +0100

    automatisation de l'install réussie

--- dolibarr/conf.php
+++ dolibarr/conf.php
@@ -1,8 +1,30 @@
 <?php
-$dolibarr_main_db_type = 'mysqli';
-$dolibarr_main_db_host = 'sae51n2_1';
+
+$dolibarr_main_url_root = 'http://localhost:8080/admin/index.php';
+$dolibarr_main_document_root = '/var/www/html/htdocs';
+$dolibarr_main_data_root = '/var/www/documents';
+$dolibarr_main_db_host = 'sae51n2_mysql_1';
 $dolibarr_main_db_port = '3306';
+$dolibarr_main_db_name = 'dolibarr_db';
 $dolibarr_main_db_user = 'dolibarr';
 $dolibarr_main_db_pass = 'dolibarr';
-$dolibarr_main_db_name = 'dolibarr_db';
-?>
+$dolibarr_main_db_type = 'mysqli';
+$dolibarr_main_db_character_set = 'utf8';
+$dolibarr_main_db_collation = 'utf8_unicode_ci';
+$dolibarr_main_db_readonly = 0;
+$dolibarr_main_lang='fr_FR';
+$dolibarr_main_authentication = 'dolibarr';
+
+$dolibarr_main_force_https = '0';
+$dolibarr_main_prod = '1';
+$dolibarr_main_restrict_os_commands = 'mysqldump, mysql, pg_dump, pgrestore';
+$dolibarr_main_restrict_ip = '';
+$dolibarr_nocsrfcheck = '0';
+$dolibarr_cron_allow_cli = '0';
+$dolibarr_mailing_limit_sendbyweb = '25';
+$dolibarr_mailing_limit_sendbycli = '0';
+
+$dolibarr_main_db_prefix = 'llx_';
+$dolibarr_main_limit_users = '0';
+$dolibarr_strict_mode = '0';
+$dolibarr_allow_download_external_modules = '0';
